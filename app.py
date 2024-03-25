75ce674266b6dfa3c2be986233dc30f479a3a9f8
vethusonamitAlgonquin <amit0004@algonquinlive.com>
2024-03-25 15:52:20 -0400

    configure application to send syslogs and created a yaml deployment

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
 from flask import Flask, redirect, url_for, session, request, jsonify
 from flask_oauthlib.client import OAuth
-import requests
+import requests, logging, logging.handlers
 
+#app name
+logapp = {'app_name':'Google SSO App'}
 app = Flask(__name__)
 app.secret_key = '"testingtesting'
 
-google_client_id = 'CLIENT_ID'
-google_client_secret = 'CLIENT_SECRET'
-google_redirect_uri = 'http://localhost:5000/login/authorized'
+# logging
+app.logger.setLevel(logging.INFO)
+handler = logging.handlers.SysLogHandler(address = ('192.168.75.133', 514)) #add vm ip here
+formatter = logging.Formatter('%(asctime)s %(app_name)s : %(message)s')
+handler.setFormatter(formatter)
+app.logger.addHandler(handler)
+app.logger = logging.LoggerAdapter(app.logger,logapp)
+# Google sso id
+google_client_id = '150659922261-t57ibajfsa3048vq3414inng2kpqac5n.apps.googleusercontent.com'
+google_client_secret = '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'
+google_redirect_uri = 'http://localhost:8000/login/authorized'
 
 oauth = OAuth(app)
 google = oauth.remote_app(
@@ -29,29 +39,35 @@ def index():
     if 'google_token' in session:
         me = google.get('userinfo')
         data = me.data
+        app.logger.info(f'{data["email"]} is logging in')
         return f'Hello {data["email"]}! <a href="/logout">Logout</a>'
+    
+    app.logger.info('User is not logged in')
     return 'Hello! Log in with your Google account: <a href="/login">Log in</a>'
 
 @app.route('/login')
 def login():
+    app.logger.info('User is trying to log in with Google account')
     return google.authorize(callback=url_for('authorized', _external=True))
 
 @app.route('/login/authorized')
 def authorized():
     response = google.authorized_response()
     if response is None or response.get('access_token') is None:
+        app.logger.info('User denied the request to sign in.')
         return 'Login failed.'
 
     session['google_token'] = (response['access_token'], '')
     me = google.get('userinfo')
     # Here, 'me.data' contains user information.
     # You can perform registration process using this information if needed.
-
+    app.logger.info(f'{me.data["email"]} is logged in')
     return redirect(url_for('index'))
 
 @app.route('/logout')
 def logout():
     session.pop('google_token', None)
+    app.logger.info('User is logged out')
     return redirect(url_for('index'))
 
 #@oauth.tokengetter
@@ -59,6 +75,7 @@ def logout():
 #    return session.get('google_token')
 @google.tokengetter
 def get_google_oauth_token():
+    app.logger.info(f'{session} is the google token')
     return session.get('google_token')
 
 
